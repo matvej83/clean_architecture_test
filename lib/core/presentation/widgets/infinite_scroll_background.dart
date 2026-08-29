ba672874a82143581ec8d415b67be94b1ec59392
Vladimir Matveenko <matvej83@gmail.com>
2026-08-29 18:15:07 +0300

    splash changed

--- lib/core/presentation/widgets/infinite_scroll_background.dart
+++ lib/core/presentation/widgets/infinite_scroll_background.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
@@ -29,18 +30,33 @@ class _InfiniteScrollingBackgroundState
     extends State<InfiniteScrollingBackground>
     with SingleTickerProviderStateMixin {
   late final AnimationController _controller;
+  bool _imagesReady = false;
+
+  Future<void> _initializeImages() async {
+    try {
+      await Future.wait([
+        precacheImage(AssetImage(widget.foregroundImage), context),
+        precacheImage(AssetImage(widget.backgroundImage), context),
+      ]);
+
+      if (!mounted) return;
+    } catch (e) {
+      log(e.toString());
+    } finally {
+      setState(() {
+        _imagesReady = true;
+      });
+    }
+  }
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(vsync: this, duration: widget.duration)
       ..repeat();
-  }
-
-  @override
-  void didChangeDependencies() {
-    precacheImage(AssetImage(widget.backgroundImage), context);
-    super.didChangeDependencies();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _initializeImages();
+    });
   }
 
   @override
@@ -58,45 +74,47 @@ class _InfiniteScrollingBackgroundState
         alignment: AlignmentDirectional.bottomCenter,
         children: [
           // Moving background
-          AnimatedBuilder(
-            animation: _controller,
-            builder: (context, child) {
-              return Positioned.fill(
-                child: Flow(
-                  delegate: _InfiniteFlowDelegate(
-                    translation: _controller.value,
-                  ),
-                  children: [
-                    // Create 3 copies
-                    _buildBackground(),
-                    _buildBackground(),
-                    _buildBackground(),
-                  ],
-                ),
-              );
-            },
-          ),
-
-          Positioned(
-            bottom: widget.foregroundImageBottomPosition ?? 0,
-            child: AnimatedBuilder(
+          if (_imagesReady)
+            AnimatedBuilder(
               animation: _controller,
               builder: (context, child) {
-                // Build Y offset cycle
-                final double offsetY =
-                    12 * math.sin(2 * math.pi * _controller.value);
-
-                return Transform.translate(
-                  offset: Offset(0, offsetY),
-                  child: Image.asset(
-                    widget.foregroundImage,
-                    width: widget.foregroundImageSize,
-                    fit: BoxFit.contain,
+                return Positioned.fill(
+                  child: Flow(
+                    delegate: _InfiniteFlowDelegate(
+                      translation: _controller.value,
+                    ),
+                    children: [
+                      // Create 3 copies
+                      _buildBackground(),
+                      _buildBackground(),
+                      _buildBackground(),
+                    ],
                   ),
                 );
               },
             ),
-          ),
+
+          if (_imagesReady)
+            Positioned(
+              bottom: widget.foregroundImageBottomPosition ?? 0,
+              child: AnimatedBuilder(
+                animation: _controller,
+                builder: (context, child) {
+                  // Build Y offset cycle
+                  final double offsetY =
+                      12 * math.sin(2 * math.pi * _controller.value);
+
+                  return Transform.translate(
+                    offset: Offset(0, offsetY),
+                    child: Image.asset(
+                      widget.foregroundImage,
+                      width: widget.foregroundImageSize,
+                      fit: BoxFit.contain,
+                    ),
+                  );
+                },
+              ),
+            ),
         ],
       ),
     );
